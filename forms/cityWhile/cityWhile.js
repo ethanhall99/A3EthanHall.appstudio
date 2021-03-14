c0873f471141fa3257d3540584ea540d64515088
Ethan Hall <ethanhall99@gmail.com>
2021-03-14 12:28:34 -0500

    V1

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,1 +1,19 @@
 // While Loop
+    
+let goOn = true
+const cityList = []
+
+while (goOn == true) {
+    let newCity = ''
+    newCity = prompt("Enter a city name:")
+    cityList.push(newCity)
+    goOn = confirm("Click OK to continue. Click Cancel to stop.")
+}
+
+
+let i = 0
+
+while (i < cityList.length) {
+    console.log(cityList[i].toLowerCase())
+    i++
+}
