b87b54508e37168f1f54fb53edda69538d171ddd
Ethan Hall <ethanhall99@gmail.com>
2021-03-14 13:16:58 -0500

    v2

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,18 @@
+// Function
+    
+let password1 = prompt("Enter password:")
+let password2 = prompt("Re-enter password:")
+
+function compare(pw1, pw2) {
+    if (pw1 === pw2)
+        return true
+    else
+        return false
+}
+
+let answer = compare(password1, password2)
+
+if (answer === true)
+    console.log("The passwords are the same.")
+else
+    console.log("The passwords are NOT the same.")
